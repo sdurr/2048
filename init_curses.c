9a221f5c4e5a84824ff9fd95e6c9dc9ff3eed789
sdurr <sdurr@e3r1p12.42.fr>
2015-02-27 23:19:05 +0100

    add draw and keypad

--- init_curses.c
+++ init_curses.c
@@ -6,16 +6,24 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 22:28:34 by sdurr             #+#    #+#             */
-/*   Updated: 2015/02/27 22:32:36 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/02/27 23:18:11 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <curses.h>
+#include "libft.h"
 
 void		init_curses(void)
 {
-	initscr();
+	WINDOW *win;
+
+	win = initscr();
 	cbreak();
 	noecho();
-	keypad(stdscr, 1);
+	if (keypad(win, 1) == ERR)
+		exit (0);
+	if (getch() == ERR)
+		exit (0);
+	//wgetnstr(win, "_", COLS);
+	endwin();
 }
